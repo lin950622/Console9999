cdbe10dd9689163a5fb37301d929e1a3c2e44867
User <User@LAPTOP-LNBV58KV>
2024-10-14 23:10:22 +0800

    加入專案檔案。

---
+++ Console9999/Console9999.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the size of the multiplication table (n): ";
+    cin >> n;
+
+    // 打印乘法表的表頭
+    cout << "   ";
+    for (int i = 1; i <= n; i++) {
+        cout << i << "\t";
+    }
+    cout << endl;
+
+    // 打印乘法表的每一行
+    for (int i = 1; i <= n; i++) {
+        cout << i << " | "; // 打印行標籤
+        for (int j = 1; j <= n; j++) {
+            cout << i * j << "\t"; // 計算並打印乘積
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
